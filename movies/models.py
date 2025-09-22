44578c44e21964160ffcfb128e1f015c94e00260
root <root@Reem>
2025-09-22 08:16:00 +0200

    feat: integrate TMDb API to fetch popular movies

--- movies/models.py
+++ movies/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 class Movie(models.Model):
-    title = models.CharField(max_length=200)
+    tmdb_id = models.IntegerField(unique=True, null=True, blank=True)
+    title = models.CharField(max_length=255)
+    overview = models.TextField(blank=True)
+    release_date = models.DateField(null=True, blank=True)
+    poster_path = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField()
-    release_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-
